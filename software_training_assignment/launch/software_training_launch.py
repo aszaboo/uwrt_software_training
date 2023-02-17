eb8353b0d9b57cca38d5604042dfb0e4d628956c
AlexSzaboDeFi <99566718+alexanderszabodefi@users.noreply.github.com>
2023-02-16 20:37:47 -0500

    launch stuff

--- software_training_assignment/launch/software_training_launch.py
+++ software_training_assignment/launch/software_training_launch.py
@@ -1,6 +1,24 @@
 import launch
+import launch_ros.actions
 from launch_ros.actions import ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
+
+def generate_launch_description():
+
+    container = ComposableNodeContainer(
+        name='software_training_container',
+        namespace='',
+        package='rclcpp_components',
+        executable='component_container',
+        composable_node_descriptions=[ComposableNode(
+            package='software_training_assignment',
+            plugin='composition::turtle_service_request_node',
+            name='turtle_request'),
+        ),
+        ], 
+        output='screen',
+    )
+return launch.LaunchDescription([container])
 
 def generate_launch_description():
 
