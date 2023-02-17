1b79e497d49ef7fc69813dc3e99a8abd256368bb
AlexSzaboDeFi <99566718+alexanderszabodefi@users.noreply.github.com>
2023-02-16 21:30:24 -0500

    updating the lunch file to include clear and reset

--- software_training_assignment/launch/software_training_launch.py
+++ software_training_assignment/launch/software_training_launch.py
@@ -5,7 +5,19 @@ from launch_ros.descriptions import ComposableNode
 
 def generate_launch_description():
 
-    container = ComposableNodeContainer(
+    clear = ComposableNodeContainer(
+        name='software_training_container',
+        namespace='',
+        package='rclcpp_components',
+        executable='component_container',
+        composable_node_descriptions=[
+            ComposableNode(
+            package='software_training_assignment',
+            plugin='composition::clear_turts',
+            name='clear_turts'),
+        ], 
+    )
+    spawn = ComposableNodeContainer(
         name='software_training_container',
         namespace='',
         package='rclcpp_components',
@@ -17,5 +29,22 @@ def generate_launch_description():
             name='spawn_turts'),
         ], 
     )
+
+    reset = ComposableNodeContainer(
+        name='software_training_container',
+        namespace='',
+        package='rclcpp_components',
+        executable='component_container',
+        composable_node_descriptions=[
+            ComposableNode(
+            package='software_training_assignment',
+            plugin='composition::reset_turts',
+            name='reset_turts'),
+        ], 
+    )
+    
         output='screen',
-return launch.LaunchDescription([container])
+return launch.LaunchDescription([
+                                clear,
+                                spawn
+]])
